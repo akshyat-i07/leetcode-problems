a27da976383fae3cba38f263b11c7a4a9b976a27
akshyat-i07 <akshyat30@gmail.com>
2026-08-09 17:00:27 +0530

    Time: 14418 ms (6.61%), Space: 19.3 MB (27.27%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,17 +1,14 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-
         def calculate(a):
             freq={}
             for ch in a:
                 freq[ch]=freq.get(ch,0)+1
-            return (max(freq.values())-min(freq.values()))
-        total=0
-
+            return(max(freq.values())-min(freq.values()))
+        ans=0
         for i in range(len(s)):
             for j in range(i,len(s)):
-                total+=calculate(s[i:j+1])
-        return total
-
-
-        
+                ans+=calculate(s[i:j+1])
+        return ans
+                
+    
